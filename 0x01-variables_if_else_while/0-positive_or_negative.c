1f869c4e33ee6e20df9dfc518106a5fcb827c55b
rayenhdr93 <hedrirayen69@gmail.com>
2021-10-01 11:38:11 +0100

    hi

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Prints "hi"
+ *
+ * Return: hi
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+printf("%d is positive", n);
+else
+if (n == 0)
+printf("%d is zero", n);
+else
+printf("%d is negative", n);
+return (0);
+}
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - "hi"
+ *
+ * Return: hi
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+return (0);
+}
