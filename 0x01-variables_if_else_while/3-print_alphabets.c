32278860e47d58063bff1c39df2996005ebfa372
rayenhdr93 <hedrirayen69@gmail.com>
2021-10-01 14:50:34 +0100

    hi

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints "hi"
+ *
+ * Return: hi
+ */
+int main(void)
+{
+int x;
+for (x = 'a'; x <= 'z' ; x++)
+{
+putchar(x);
+}
+for (x = 'A'; x <= 'Z' ; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
