82b0dbfe150e65aeee644e5c2060a2c66b5f223c
rayenhdr93 <hedrirayen69@gmail.com>
2021-10-01 20:56:11 +0100

    hi

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Prints "hi"
+ *
+ * Return: hi
+ */
+int main(void)
+{
+int x, y, z, r;
+for (x = '0'; x <= '9'; x++)
+{
+for (y = '0'; y <= '9'; y++)
+{
+for (z = x; z <= '9' ; z++)
+{
+for (r = y + 1; r <= '9'; r++)
+{
+if (r > 1)
+{
+putchar(',');
+putchar(' ');
+}
+putchar(x);
+putchar(y);
+putchar(' ');
+putchar(z);
+putchar(r);
+}
+}
+}
+}
+putchar('\n');
+}
