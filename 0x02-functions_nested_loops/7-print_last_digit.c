0eb5c4afdea6fd0c8fa72c5fdfef176a38d27378
rayenhdr93 <hedrirayen69@gmail.com>
2021-10-05 15:50:34 +0100

    hi

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - hi
+ * @c: hi
+ *
+ * Return: hi
+ */
+int print_last_digit(int)
+{
+	int x;
+	x = print_last_digit % 10;
+	return(_putchar(x));
+}
