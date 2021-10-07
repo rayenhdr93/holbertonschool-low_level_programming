de3c56295fef5f627919e9cd72a92a9b3c8bc13f
rayenhdr93 <hedrirayen69@gmail.com>
2021-10-07 16:39:56 +0100

    hi

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - hi
+ * @n: hi
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n > 0)
+	{
+		for(x = 0; x <= n; x++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
