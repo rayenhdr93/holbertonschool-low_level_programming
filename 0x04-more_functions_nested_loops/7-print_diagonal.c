00428fe49a94613f2ac85b1b25e84db20b152302
rayenhdr93 <hedrirayen69@gmail.com>
2021-10-07 16:46:08 +0100

    hi

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - hi
+ * @n: hi
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n > 0)
+	{
+		for (x = 1; x < n; x++)
+		{
+			_putchar(' ');
+		}
+	_putchar('\\');
+	}
+	_putchar('\n');
+}
