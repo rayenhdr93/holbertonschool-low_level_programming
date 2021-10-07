69d8ace16f5895b9f372eac5d2e9f2f6010686ba
rayenhdr93 <hedrirayen69@gmail.com>
2021-10-07 09:32:35 +0100

    hi

---
+++ 0x03-debugging/pos.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "main.h"
+/**
+ * positive_or_negative - hi
+ *
+ * Return: hi
+ */
+void positive_or_negative(int i)
+{
+srand(time(0));
+i = rand() - RAND_MAX / 2;
+if (i > 0)
+printf("%d is positive\n", i);
+else
+if (i == 0)
+printf("%d is zero\n", i);
+else
+printf("%d is negative\n", i);
+return (0);
+}
