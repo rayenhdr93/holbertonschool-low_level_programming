bf243f88af6002d28aee5e746cc9b3fa5a2d5440
rayenhdr93 <hedrirayen69@gmail.com>
2021-10-16 17:32:07 +0100

    hi

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,32 +1,36 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
 /**
  * print_number - hi
  * @n: hi
  */
 void print_number(int n)
 {
-	int x, i, t, j;
-	char ch[100];
+	int x, i, n1, d, r;
 
 	i = 0;
-	t = n;
+	n1 = n;
 	if (n == 0)
 	_putchar(n + 48);
 	else
 	if (n < 0)
 	{
 	_putchar('-');
-	t = -t;
+	n1 = -n1;
 	}
-	while (t > 0)
+	d = 1;
+	r = 11;
+	while (r >= 10)
 	{
-		x = t % 10;
-		t = t / 10;
-		ch[i] = x + 48;
+	d = d * 10;
+	r = n1 / d;
+	}
+	while (n1 > 0)
+	{
+		x = n1 / d;
+		n1 = n1 % d;
+		_putchar(x + 48);
+		d = d / 10;
 		i++;
 	}
-	for (j = (i - 1); j >= 0; j--)
-		_putchar(ch[j]);
 }
