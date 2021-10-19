dcade1fa6f1e47924d095b0393fea290211eb853
rayenhdr93 <hedrirayen69@gmail.com>
2021-10-19 18:01:59 +0100

    hi

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strstr - hi
+ * @haystack: hi
+ * @needle: hi
+ * Return: hi
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i, j, x, y;
+	int a;
+
+	j = 0;
+	t =
+	x = strlen(haystack);
+	y = strlen(needle);
+	for (i = 0; i < y; i++)
+	{
+	for (j = 0; j < (x - y); j++)
+	{
+		if (haystack[j] == needle[i])
+		{
+			
+		}
+	}
+	}
+	return (haystack);
+
+}
