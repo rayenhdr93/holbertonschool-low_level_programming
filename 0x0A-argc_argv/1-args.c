12342e70cf496ad1cf7dd3ea2bae899a06de0abe
rayenhdr93 <hedrirayen69@gmail.com>
2021-10-22 11:34:31 +0100

    hi

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "stdlib.h"
+#include "stdio.h"
+/**
+ * main - hi
+ * @argc: hi
+ * @argv: hi
+ * Return: hi
+ */
+int main(int argc, char *argv[])
+{
+	if (*argv[0] != '\0')
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
