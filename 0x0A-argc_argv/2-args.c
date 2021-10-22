5768a5f7a36404f7ed7b793c46e37091a65c786d
rayenhdr93 <hedrirayen69@gmail.com>
2021-10-22 11:39:09 +0100

    hi

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "stdlib.h"
+#include "stdio.h"
+/**
+ * main - hi
+ * @argc: hi
+ * @argv: hi
+ * Return: hi
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (i = 0; i < argc; i++)
+	printf("%s\n", argv[i]);
+	return (0);
+}
+
