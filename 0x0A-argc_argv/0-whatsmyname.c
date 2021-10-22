43ae2e846853aff2658a27b0f5cd62868e279f82
rayenhdr93 <hedrirayen69@gmail.com>
2021-10-22 11:12:12 +0100

    hi

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "stdlib.h"
+#include "stdio.h"
+/**
+ * main - hi
+ * @argc: hi
+ * @argv: hi
+ * Return: hi
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
+
