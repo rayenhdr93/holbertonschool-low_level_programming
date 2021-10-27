a6142fec67a34342802150f07b303f1a309dc872
rayenhdr93 <hedrirayen69@gmail.com>
2021-10-27 14:34:01 +0100

    hi

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "string.h"
+/**
+ * _strdup - hi
+ * @str: hi
+ * Return: hi
+ */
+char *_strdup(char *str)
+{
+	char *ch;
+	int x, i;
+
+	x = strlen(str);
+	if (x == 0)
+		return (NULL);
+	ch = malloc(sizeof(char) * x);
+	if (ch == NULL)
+		return (NULL);
+	for (i = 0; i < x; i++)
+		ch[i] = str[i];
+	return (ch);
+}
