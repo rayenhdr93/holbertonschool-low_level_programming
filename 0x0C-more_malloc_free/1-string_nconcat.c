cb0ca20918078914b9eaaf048b2e311a61647a2d
rayenhdr93 <hedrirayen69@gmail.com>
2021-10-28 13:37:46 +0100

    hi

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,7 +16,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	y = strlen(s2);
 	if (n >= y)
 		n = y;
-	ch = malloc(x + n + 1);
+	ch = malloc(x + n);
 	if (!ch)
 		return (NULL);
 	for (i = 0; i < (x + n); i++)
