e90876f8f951bad8eb4ca0a4bece67c3c5cd8a3e
rayenhdr93 <hedrirayen69@gmail.com>
2021-10-28 18:38:44 +0100

    hi

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "stdlib.h"
+/**
+ * array_range - hi
+ * @min: hi
+ * @max: hi
+ * Return: hi
+ */
+int *array_range(int min, int max)
+{
+	int *p;
+	int i, j;
+
+	if (min > max)
+		return (NULL);
+	p = malloc(sizeof(int) * (1 + (max - min)));
+	if (!p)
+		return (NULL);
+	j = 0;
+	for (i = min; i <= max; i++)
+	{
+		j++;
+		p[j] = i;
+	}
+	return (p);
+}
