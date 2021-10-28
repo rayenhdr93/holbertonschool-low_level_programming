fccd62b31b82ffdff72d612ea6894d267e94f7ce
rayenhdr93 <hedrirayen69@gmail.com>
2021-10-28 16:30:08 +0100

    hi

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,5 @@
 #include "stdlib.h"
+#include "string.h"
 /**
  * _calloc - hi
  * @nmemb: hi
@@ -8,15 +9,11 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *p = malloc(size * nmemb);
-	unsigned int i;
 
 	if (!p)
 		return (NULL);
 	if ((size == 0) || (nmemb == 0))
 		return (NULL);
-	for (i = 0; i < size; i++)
-	{
-		p[i] = 0;
-	}
+	memset(p, 0, size);
 	return (p);
 }
