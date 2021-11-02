3e3becb5fe45789f76519f2d4abc10d5330da878
rayenhdr93 <hedrirayen69@gmail.com>
2021-11-02 03:28:15 +0100

    hi

--- 0x0D-preprocessor/3-main.c
+++ 0x0D-preprocessor/3-main.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include "3-function_like_macro.h"
-#include "3-function_like_macro.h"
 
 /**
- * main - check the code
+ * main - check the code .
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int i;
-    int j;
-
-    i = ABS(-0) * 10;
-    j = ABS(0) * 10;
-    printf("%d, %d\n", i, j);
-    return (0);
+#ifndef ABS
+	printf("The macro 'ABS' is not defined\n");
+	return (1);
+#else
+	printf("%d\n", ABS(1024 - 2048));
+	return (0);
+#endif
 }
