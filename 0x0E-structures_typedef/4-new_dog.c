e97a6e341a1db802e5f95f3c8b13fe5772ce2f54
rayenhdr93 <hedrirayen69@gmail.com>
2021-11-02 09:59:31 +0100

    hi

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,10 +11,14 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *x;
+	char *a;
+	char *b;
 
 	if ((name != NULL) && (age >= 0) && (owner != NULL))
 	{
 		x = malloc(sizeof(struct dog));
+		a = strndup(x->name, sizeof(x->name));
+		b = strndup(x->owner, sizeof(x->owner));
 		if (!x)
 		{
 			free(x);
