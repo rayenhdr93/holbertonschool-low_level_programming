82a2764cb941e8e7d78677f68ca96eb210585d26
rayenhdr93 <hedrirayen69@gmail.com>
2021-11-03 17:27:06 +0100

    hi

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "function_pointers.h"
 /**
  * print_name - hi
@@ -6,5 +7,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name != NULL)
 	(*f)(name);
 }
