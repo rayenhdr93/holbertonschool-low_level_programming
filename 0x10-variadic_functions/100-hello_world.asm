7b6d9254834493425a72c37dd6f6f0bf0817ac4c
rayenhdr93 <hedrirayen69@gmail.com>
2021-11-07 17:59:51 +0100

    hi

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -1,17 +1,26 @@
-.model tiny                              ;code, data & program in 1 64K segment
+section .text                   ;section declaration
 
-.code                                       ;code segment
+                                ;we must export the entry point to the ELF linker or
+    global  main              ;loader. They conventionally recognize _start as their
+			                          ;entry point. Use ld -e foo to override the default.
 
-MAIN PROC
-                       
-    MOV AH, 09h                    ;function to display a string
-    MOV DX, offset message  ;offset of Message string terminated with $
+main:
 
-    int 21h                                 ;DOS interrupt
-    MOV AH, 4ch                    ;funtion to terminate
-    int 21h                                 ;DOS interrupt
+                                ;write our string to stdout
 
-ENDP
-message db "Hello World $"   ;Message to be displayed terminated with $(indicates end of string)
+    mov     edx,len             ;third argument: message length
+    mov     ecx,msg             ;second argument: pointer to message to write
+    mov     ebx,1               ;first argument: file handle (stdout)
+    mov     eax,4               ;system call number (sys_write)
+    int     0x80                ;call kernel
 
-END MAIN
+                                ;and exit
+
+  	mov     ebx,0               ;first syscall argument: exit code
+    mov     eax,1               ;system call number (sys_exit)
+    int     0x80                ;call kernel
+
+section .data                   ;section declaration
+
+msg db      "Hello, world",0xa ;our dear string
+len equ     $ - msg             ;length of our dear string
