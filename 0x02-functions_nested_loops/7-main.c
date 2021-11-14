da0be3ff9053582b97cff5a51c350b94564800a0
rayenhdr93 <hedrirayen69@gmail.com>
2021-11-14 21:27:38 +0100

    hi

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "stdio.h"
 
 /**
  * main - check the code
@@ -7,12 +8,11 @@
  */
 int main(void)
 {
-    int r;
-
     print_last_digit(98);
+ 	printf("\n");
     print_last_digit(-2147483648);
-    r = print_last_digit(-1024);
-    _putchar('0' + r);
-    _putchar('\n');
+    printf("\n");
+    print_last_digit(55893);
+    printf("\n");
     return (0);
 }
