3ba3085860b711c44d4be2724004c313d3657778
rayenhdr93 <hedrirayen69@gmail.com>
2021-11-17 16:34:06 +0100

    hi

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,5 +11,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	for (i = 0; (i < index && head->next != NULL); i++)
 	head = head->next;
+	if (index > i + 1)
+	return (NULL);
+	else
 	return (head);
 }
