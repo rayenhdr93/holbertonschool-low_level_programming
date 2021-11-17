6df36ab952ca2e48117bd8810d530d8d7bb1356b
rayenhdr93 <hedrirayen69@gmail.com>
2021-11-17 14:50:47 +0100

    hi

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - hi
+ * @head: hi
+ * Return: the number of nodes
+ */
+void free_listint2(listint_t **head)
+{
+	if (*head == NULL)
+		return;
+	while ((*head)->next != NULL)
+	{
+		free(*head);
+		*head = (*head)->next;
+	}
+	free(*head);
+	*head = NULL;
+}
