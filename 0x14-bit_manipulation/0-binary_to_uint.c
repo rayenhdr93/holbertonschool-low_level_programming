4210d715faec8bc163f4d42f35e6afd442237062
rayenhdr93 <hedrirayen69@gmail.com>
2021-11-18 15:08:57 +0100

    hi

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * binary_to_uint - hi
+ * @b: char
+ * Return: hi
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int unint = 0;
+	int pow, i;
+
+	pow = 1;
+	i = strlen(b);
+	while (i > 0)
+	{
+		i--;
+		if (b[i] == '1')
+		unint = unint + pow;
+		else if (b[i] != '0')
+		return (0);
+		pow *= 2;
+	}
+	return (unint);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,8 @@
+#ifndef main_h
+#define main_h
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+unsigned int binary_to_uint(const char *b);
+#endif
