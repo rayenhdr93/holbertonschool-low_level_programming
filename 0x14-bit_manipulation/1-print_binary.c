d84884497f1e1b56d9a92f79c9792f775d80a4b2
rayenhdr93 <hedrirayen69@gmail.com>
2021-11-18 16:41:05 +0100

    did u expect me to do this in the first day of bit manipulation ? r u crazy ?

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * power2 - hi
+ * @n: hi
+ * Return: hi
+ */
+unsigned long int power2(unsigned long int n)
+{
+	unsigned long int x = 1;
+
+	while (x < n)
+	x *= 2;
+	if (x > n)
+	x = x >> 1;
+	return (x);
+}
+/**
+ * print_binary - hi
+ * @n: hi
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int i, x;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	else if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+	x = power2(n);
+	for (i = x; i > 0; i = i >> 1)
+	{
+		if (n & i)
+		_putchar('1');
+		else
+		_putchar('0');
+	}
+}
