b4ed5258f3335c453d7fe27d3da47e189f30dc89
rayenhdr93 <hedrirayen69@gmail.com>
2021-11-19 11:57:03 +0100

    omg..

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,12 +11,12 @@ int clear_bit(unsigned long int *n, unsigned int index)
 
 	if (index > 34)
 		return (-1);
-	i = i << index;
+	i = i << (index - 1);
 	printf("n = %ld\n", *n);
 	printf("i = %d\n", i);
 	if (i <= *n)
 	{
-	*n = *n - (i + 1);
+	*n = *n - i;
 	return (1);
 	}
 	else
