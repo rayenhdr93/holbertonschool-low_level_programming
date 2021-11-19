d29fa0f0c1a95c72fa41fb27ac1b606d3c2e59c8
rayenhdr93 <hedrirayen69@gmail.com>
2021-11-19 13:22:31 +0100

    hi

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,7 +13,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	int c = 0;
 
 	if ((n == ULONG_MAX) && (m == 0))
-	return(64);
+		return (64);
 	x = n ^ m;
 	c = x; 
 	while (x > 0)
