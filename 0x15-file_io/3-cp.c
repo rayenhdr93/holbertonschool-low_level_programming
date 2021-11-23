7f642d611b2ffe41c66609f3aa3e7474ca7f6e63
rayenhdr93 <hedrirayen69@gmail.com>
2021-11-23 14:01:47 +0100

    hi

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -84,8 +84,8 @@ char *readfromfile(char *filename)
 	y = close(x);
 	if (y == -1)
 	{
-		exit(100);
 		printf("Error: Can't close fd %d", x);
+		exit(100);
 	}
 	return (c);
 }
@@ -120,8 +120,8 @@ void writetofile(char *filename, char *ch)
 	y = close(x);
 	if (y == -1)
 	{
-		exit(100);
 		printf("Error: Can't close fd %d", x);
+		exit(100);
 	}
 }
 /**
