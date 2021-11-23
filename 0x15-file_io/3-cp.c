084c3da582ec5b714578c64f16df3c184dfe635a
rayenhdr93 <hedrirayen69@gmail.com>
2021-11-23 13:52:54 +0100

    hi

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,58 @@
 #include "main.h"
 /**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ *
+ * @dest: destination
+ * @src: source
+ * @n: size
+ *
+ * Return: pointer to dest
+ **/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i);
+
+	return (dest);
+}
+/**
+ * _realloc - reallocates a memory block
+ * @ptr: old mem
+ * @old_size: old_size
+ * @new_size: new_size
+ *
+ * Return: new mem
+ **/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *nptr;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		nptr = malloc(new_size);
+		return (nptr);
+	}
+
+	if (!new_size)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	nptr = malloc(old_size + (new_size - old_size));
+
+	_memcpy(nptr, ptr, old_size);
+
+	free(ptr);
+
+	return (nptr);
+}
+/**
  * readfromfile - hi
  * @filename: hi
  * Return: hi
@@ -25,8 +78,8 @@ char *readfromfile(char *filename)
 			exit(98);
 			printf("Error: Can't read from");
 		}
-		c = realloc(c, i);
 		i *= 2;
+		c = _realloc(c, i - 1024, i);
 	}
 	close(x);
 	return (c);
