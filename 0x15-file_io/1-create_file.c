472b50b6995f982cb9eba15127a338c28fdc60b0
rayenhdr93 <hedrirayen69@gmail.com>
2021-11-23 10:26:35 +0100

    hi

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * create_file - hi
+ * @filename: hi
+ * @text_content: hi
+ * Return: hi
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int x = 0, y;
+
+	if (filename)
+		return (-1);
+	x = open(filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+	if (x == -1)
+		return (-1);
+	y = write(x, text_content, strlen(text_content));
+	if (y == -1)
+		return (-1);
+	close(x);
+	return (1);
+}
