2ad07f1794717899904f289343fa523cf8156a3b
rayenhdr93 <hedrirayen69@gmail.com>
2021-11-24 10:24:33 +0100

    hi

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,127 +1,39 @@
 #include "main.h"
 /**
- * _memcpy - copies n bytes from memory area src to memory area dest
- *
- * @dest: destination
- * @src: source
- * @n: size
- *
- * Return: pointer to dest
- **/
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		*(dest + i) = *(src + i);
-
-	return (dest);
-}
-/**
- * _realloc - reallocates a memory block
- * @ptr: old mem
- * @old_size: old_size
- * @new_size: new_size
- *
- * Return: new mem
- **/
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-{
-	void *nptr;
-
-	if (new_size == old_size)
-		return (ptr);
-
-	if (ptr == NULL)
-	{
-		nptr = malloc(new_size);
-		return (nptr);
-	}
-
-	if (!new_size)
-	{
-		free(ptr);
-		return (NULL);
-	}
-
-	nptr = malloc(old_size + (new_size - old_size));
-
-	_memcpy(nptr, ptr, old_size);
-
-	free(ptr);
-
-	return (nptr);
-}
-/**
  * readfromfile - hi
- * @filename: hi
+ * @x: hi
  * Return: hi
  */
-char *readfromfile(char *filename)
+char *readfromfile(char x)
 {
-	char *c = malloc(1024);
-	int x = 0, y = 1024, i = 1024;
+	char *c;
+	int y = 0;
 
-	if (filename == NULL)
-		return (0);
-	x =	open(filename, O_RDONLY);
-	if (x == -1)
+	c = malloc(1024);
+	y = read(x, c, 1024);
+	if (y == -1)
 	{
 		exit(98);
 		printf("Error: Can't read from");
-	}
-	while (y == 1024)
-	{
-		y = read(x, c, 1024);
-		if (y == -1)
-		{
-			exit(98);
-			printf("Error: Can't read from");
-		}
-		i *= 2;
-		c = _realloc(c, i - 1024, i);
-	}
-	y = close(x);
-	if (y == -1)
-	{
-		printf("Error: Can't close fd %d", y);
-		exit(100);
 	}
 	return (c);
 }
 /**
  * writetofile - hi
- * @filename: hi
+ * @x: hi
  * @ch: hi
  * Return: hi
  */
-void writetofile(char *filename, char *ch)
+void writetofile(char x, char *ch)
 {
-	int x = 0, y;
+	int y = 0;
 
-	if (!filename)
-	{
-		exit(99);
-		printf("Error: Can't write to");
-	}
-	x = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (x == -1)
-	{
-		exit(99);
-		printf("Error: Can't write to");
-	}
 	if (ch)
 		y = write(x, ch, strlen(ch));
 	if (y == -1)
 	{
 		exit(99);
 		printf("Error: Can't write to");
-	}
-	y = close(x);
-	if (y == -1)
-	{
-		printf("Error: Can't close fd %d", y);
-		exit(100);
 	}
 }
 /**
@@ -133,14 +45,49 @@ void writetofile(char *filename, char *ch)
 int main(int ac, char **av)
 {
 	char *ch;
+	int x = 0, y = 0, a = 0, b = 0, l = 1024;
 
 	if (ac != 3)
 	{
 		exit(97);
 		printf("Usage: cp file_from file_to");
 	}
-	ch = readfromfile(av[1]);
-	writetofile(av[2], ch);
-	free(ch);
+		if (av[1] == NULL)
+		return (0);
+	x =	open(av[1], O_RDONLY);
+	if (x == -1)
+	{
+		exit(98);
+		printf("Error: Can't read from");
+	}
+		if (!av[2])
+	{
+		exit(99);
+		printf("Error: Can't write to");
+	}
+	a = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (a == -1)
+	{
+		exit(99);
+		printf("Error: Can't write to");
+	}
+	while (l == 1024)
+	{
+	ch = readfromfile(x);
+	writetofile(a, ch);
+	l = strlen(ch);
+	}
+	y = close(x);
+	if (y == -1)
+	{
+		printf("Error: Can't close fd %d", y);
+		exit(100);
+	}
+	b = close(a);
+	if (b == -1)
+	{
+		printf("Error: Can't close fd %d", b);
+		exit(100);
+	}
 	return (0);
 }
