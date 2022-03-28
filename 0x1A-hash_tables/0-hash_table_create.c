4a626202b37385ec02cf25675a45ed80588fc1e0
rayenhdr93 <hedrirayen69@gmail.com>
2022-03-28 09:17:13 +0000

    0-

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,7 +6,10 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht = malloc(sizeof(ht) * size);
+	hash_table_t *ht = malloc(sizeof(ht));
 	
+	if (!ht)
+		return (NULL);
+	ht->size = size;
 	return ht;
 }
