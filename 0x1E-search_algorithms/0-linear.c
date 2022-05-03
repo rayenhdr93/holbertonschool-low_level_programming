fcb1754a8df791b77bc7eba1790a57399147f21c
rayenhdr93 <hedrirayen69@gmail.com>
2022-05-03 18:51:20 +0100

    0-

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,13 +1,22 @@
 #include "search_algos.h"
-int linear_search(int *array, size_t size, int value){
+/**
+ * linear_search - func
+ * @array: a
+ * @size: s
+ * @value: v
+ * Return: int
+ */
+int linear_search(int *array, size_t size, int value)
+{
 	size_t i;
 
 	if (!array)
-		return(-1);
-	for (i = 0; i < size; i++){
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
 		printf("Value checked array [%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
-	return(-1);
+	return (-1);
 }
